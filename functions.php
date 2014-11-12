d7956b1f208a279b8854c290ddc28f4382e92066
Edward Caissie <edward.caissie@gmail.com>
2014-11-11 21:58:41 -0500

    - Removed `@import` from the main stylesheet - Enqueued parent-theme stylesheet in place of `@import` - PHPDocs updates

--- functions.php
+++ functions.php
@@ -33,12 +33,44 @@
  * @version     0.2
  * @date        July 20, 2014
  * Modified to use filtered values instead of $opus_defaults class object
+ *
+ * @version     1.1
+ * @date        November 11, 2014
+ * Enqueue `Opus Primus` (parent-theme) stylesheet
+ */
+
+/**
+ * Styles and Scripts
+ * Main styles and scripts to be enqueued for the theme
+ *
+ * @package  Cais
+ * @since    1.1
+ *
+ * @uses     get_template_directory_uri
+ * @uses     wp_enqueue_style
+ *
+ * @internal hooked to `wp_enqueue_scripts` action
+ */
+function cais_styles_and_scripts() {
+	wp_enqueue_style( 'parent-theme-opus-primus', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'cais_styles_and_scripts' );
+
+
+/**
+ * Number of Secondary Images Value
+ * Set the number of secondary images
+ *
+ * @package  Cais
+ * @since    1.0
+ *
+ * @internal hooked into `opus_number_of_secondary_images_value` filter
+ *
+ * @return int
  */
 function cais_number_of_secondary_images_value() {
 	return 5;
 }
 
 add_filter( 'opus_number_of_secondary_images_value', 'cais_number_of_secondary_images_value' );
-
-/** Revert Pagination Feature to plain text links */
-/** add_filter( 'opus_navigation_pagination', array( 'OpusPrimusNavigation', 'posts_link' ) ); */
